81fd81dcc3eef763cdb62436d0c9d9ebf6d2a744
Bryce Northrup <aznorthrup@gmail.com>
2022-06-23 08:37:28 -0700

    Moved all aspects of Home Controller into Main Controller and delete Home Controller, added a terminology page

--- src/main/java/com/brycenorthrup/lockpickingapp/controllers/MainController.java
+++ src/main/java/com/brycenorthrup/lockpickingapp/controllers/MainController.java
@@ -3,13 +3,26 @@ package com.brycenorthrup.lockpickingapp.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	private Logger logger = LoggerFactory.getLogger(LoggerController.class);
-	
+
+	@GetMapping(path = "/")
+	public String showHomePage(Model model) {
+		logger.info("Displayed home page");
+		return "home.html";
+	}
+
+	@GetMapping(path = "/terminology")
+	public String showTermsPage(Model model) {
+		logger.info("Displayed terminology page");
+		return "terms.html";
+	}
+
 	@GetMapping("/login")
 	public String login() {
 		logger.info("Displayed login page");
